271153018514180a2b5998f07b10f013705c5192
Tom <thomas.spencer@codurance.com>
2021-06-19 15:09:46 +0100

    add Binary operator

--- src/com/codurance/functionalProgramming/FunctionalInterfaceExample.java
+++ src/com/codurance/functionalProgramming/FunctionalInterfaceExample.java
@@ -1,5 +1,10 @@
 package com.codurance.functionalProgramming;
 
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
 public class FunctionalInterfaceExample {
   @FunctionalInterface
   public interface Predicate<T> {
@@ -25,5 +30,31 @@ public class FunctionalInterfaceExample {
 
     System.out.println(b1.test("chicken", "chick"));
     System.out.println(b2.test("chicken", "chick"));
+
+    Function<String, Integer> f1 = String::length;
+    Function<String, Integer> f2 = x -> x.length();
+
+    System.out.println(f1.apply("cluck"));
+    System.out.println(f1.apply("cluck"));
+
+    BiFunction<String, String, String> b3 = String::concat;
+
+    BiFunction<String, String, String> b4 = (string, toAdd) -> string.concat(toAdd);
+
+    System.out.println(b3.apply("baby ", "chick"));
+    System.out.println(b4.apply("baby ", "chick"));
+
+    UnaryOperator<String> u1 = String::toUpperCase;
+    UnaryOperator<String> u2 = x -> x.toUpperCase();
+
+    System.out.println(u1.apply("chirp"));
+    System.out.println(u2.apply("chirp"));
+
+    BinaryOperator<String> b5 = String::concat;
+    BinaryOperator<String> b6 = (string, toAdd) -> string.concat(toAdd);
+
+    System.out.println(b5.apply("baby ", "chick"));
+    System.out.println(b6.apply("baby ", "chick"));
+
   }
 }
