a48ac20cfe507e504d4551ff46fe794da2c17533
Tom <thomas.spencer@codurance.com>
2021-08-09 18:39:21 +0100

    add HTMLParser

---
+++ katas/pablo-a/HTMLParser/src/main/java/com/codurance/Parser.java
@@ -0,0 +1,10 @@
+package com.codurance;
+
+public class Parser {
+  public HTMLNode parse(String input) throws InvalidHTMLException {
+    if (input == null || input.isEmpty()) {
+      throw new InvalidHTMLException();
+    }
+    return new HTMLNode();
+  }
+}
---
+++ katas/pablo-a/HTMLParser/src/test/java/com/codurance/ParserShould.java
@@ -0,0 +1,44 @@
+package com.codurance;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class ParserShould {
+  @Test
+  void throw_invalid_html_exception_for_null() {
+    final Parser parser = new Parser();
+
+    assertThrows(InvalidHTMLException.class, () -> {
+      parser.parse(null);
+    });
+  }
+
+  @Test
+  void throw_invalid_html_exception_for_empty_string() {
+    final Parser parser = new Parser();
+
+    assertThrows(InvalidHTMLException.class, () -> {
+      parser.parse("");
+    });
+  }
+
+  @Test
+  void html_with_root_html_node_returns_html_node() throws InvalidHTMLException {
+    final Parser parser = new Parser();
+    assertThat(parser.parse("<html></html>")).isInstanceOf(HTMLNode.class);
+  }
+
+
+//<html>
+//  <head>
+//    <meta charset="utf-8">
+//    <title></title>
+//  </head>
+//  <body>
+//
+//  </body>
+//</html>
+}
