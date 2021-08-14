8dff3ed3b3265724881332047d46d3cadbe938ee
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2021-08-14 09:41:05 +0100

    add notes for intermediate

--- sql/sqlpracticeproblems/intermediate_problems.sql
+++ sql/sqlpracticeproblems/intermediate_problems.sql
@@ -17,3 +17,26 @@ GROUP BY c.country, c.City
 ORDER BY total_customers DESC;
 
 
+-- 22. Products need reordering
+-- What products do we have in our inventory
+-- that should be reordered? For now, just use the
+-- fields UnitsInStock and ReorderLevel where UnitsInStock
+-- is less than or equal to the ReorderLevel, Ignore the
+-- fields UnitsOnOrder and Discontinued
+
+SELECT ProductID, ProductName, UnitsInStock, ReorderLevel
+from products
+where UnitsInStock <= ReorderLevel
+order by productID asc;
+
+-- 23. Products that need reordering, continued
+-- Now we need to incorporate these fields -- UnitsInSttock, UnitsOnOrder
+-- ReorderLevel, Discontinued -- into our calculation. We'll define 'products that
+-- need reordering' with the following:
+-- UnitsInStock plus UnitsOnOrder are less than or equal to ReorderLevel
+-- The Discontinued flag is false (0)
+
+SELECT ProductID as "Product ID", ProductName as "Product Name", UnitsInStock as "Units In Stock", UnitsOnOrder as "Units on Order", ReorderLevel as "Reorder Level", Discontinued
+from products
+where (UnitsInStock + UnitsOnOrder) <= ReorderLevel
+and Discontinued is false;
