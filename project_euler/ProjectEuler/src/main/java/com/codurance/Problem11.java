56615d253cafef2e1810b189e06c3fa2f6df8166
Tom <thomas.spencer@codurance.com>
2021-09-09 21:09:07 +0100

    add left down

---
+++ project_euler/ProjectEuler/src/main/java/com/codurance/Problem11.java
@@ -0,0 +1,52 @@
+package com.codurance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Position {
+  final int x;
+  final int y;
+
+  public Position(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+public class Problem11 {
+  private final List<List<Integer>> numbers;
+  private Position position = new Position(0, 0);
+
+  public Problem11(List<List<Integer>> numbers) {
+    this.numbers = numbers;
+  }
+
+
+  public Integer getRightDownProduct(Position position) {
+    if (position.x > 16 || position.y > 16) {
+      return 0;
+    }
+
+    List<Integer> list = new ArrayList<Integer>();
+    list.add(numbers.get(position.y).get(position.x));
+    list.add(numbers.get(position.y + 1).get(position.x + 1));
+    list.add(numbers.get(position.y + 2).get(position.x + 2));
+    list.add(numbers.get(position.y + 3).get(position.x + 3));
+
+    return list.stream().reduce(1, (a, b) -> a * b);
+  }
+
+  public Integer getLeftDownProduct(Position position) {
+    if (position.x < 3 || position.x > 19 || position.y > 16) {
+      return 0;
+    }
+
+    List<Integer> list = new ArrayList<>();
+    list.add(numbers.get(position.y).get(position.x));
+    list.add(numbers.get(position.y + 1).get(position.x - 1));
+    list.add(numbers.get(position.y + 2).get(position.x - 2));
+    list.add(numbers.get(position.y + 3).get(position.x - 3));
+
+    return list.stream().reduce(1, (a, b) -> a * b);
+  }
+}
